30546920fea65bcaad5767ba855eb87d6ac07e0c
maheshwaghmare <mwaghmare7@gmail.com>
2019-12-31 12:03:25 +0530

    Updates

---
+++ precache-manifest.5af401931cab47ef88a6c404cfdce945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a574fa803b69b2c03247db04e3520067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e30cd5dbcb904e62e09c",
+    "url": "/static/css/main.10508410.chunk.css"
+  },
+  {
+    "revision": "8775309d419791552dcc",
+    "url": "/static/js/2.361a1d1f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.361a1d1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e30cd5dbcb904e62e09c",
+    "url": "/static/js/main.71c5eebf.chunk.js"
+  },
+  {
+    "revision": "820e6725dc842cb4dec4",
+    "url": "/static/js/runtime-main.969b7561.js"
+  }
+]);
